6b7a6085d55e689cc9c45b28bebd77b8da466f26
Gauransh <139785408+gauransh09@users.noreply.github.com>
2026-03-24 23:19:20 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const client = require('prom-client');
 
+const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/', (req, res) => {
+  res.send('DevOps Pipeline Working ');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
